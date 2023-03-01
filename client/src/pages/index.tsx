0baf22c4f59a88d10460d2802ffb738c354c9eef
DaYeezus <mohammadrezajafari.dev@gmail.com>
2023-03-01 19:04:47 +0330

    done client index design

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,9 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/Navbar";
+import DownloadBox from "@/components/DownloadBox";
+import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex items-center flex-col">
+      <Navbar />
+      <main className="w-full my-20 px-5 md:px-24 lg:px-44">
+        <Hero />
+        <DownloadBox />
+      </main>
+    </div>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      roboto: ["Roboto"],
+      roboto_italic: ["Roboto italic"],
+      roboto_bold: ["Roboto Bold"],
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
