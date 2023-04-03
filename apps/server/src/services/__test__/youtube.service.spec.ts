c615baaec5615b5d30528822836cd28135dcc46f
DaYeezus <mohammadrezajafari.dev@gmail.com>
2023-04-03 11:17:14 +0330

    Remove client project.

--- apps/server/src/services/__test__/youtube.service.spec.ts
+++ apps/server/src/services/__test__/youtube.service.spec.ts
@@ -1,130 +1,235 @@
 import {
-  downloadAudioFromPlaylist,
-  downloadSingleAudio,
   getPlaylistItemsUrls,
   getYoutubeContentInfo,
   getYoutubePlaylistInfo,
 } from '../youtube/youtube.service';
-import {videoInfo} from 'ytdl-core';
-import {firstValueFrom} from 'rxjs';
-import {afterAll, assertType, beforeAll, describe, expect, test,} from 'vitest';
-import {DownloadedAudio} from '../../interfaces/download.interface';
-import {redisClient} from '../../conf/redis.conf';
-import {config} from 'dotenv';
-import {youtubePlayListResponse} from '../../interfaces/youtube-playlist.interface';
-import {unlinkSync} from 'fs';
+import ytdl, { videoInfo } from 'ytdl-core';
+import { firstValueFrom } from 'rxjs';
+import {
+  afterAll,
+  assertType,
+  beforeAll,
+  describe,
+  expect,
+  test,
+} from 'vitest';
+import {
+  DownloadedAudio,
+  DownloadedVideo,
+} from '../../interfaces/download.interface';
+import { redisClient } from '../../conf/redis.conf';
+import { config } from 'dotenv';
+import { youtubePlayListResponse } from '../../interfaces/youtube-playlist.interface';
+import { unlink, unlinkSync } from 'fs';
+import {
+  downloadAudioFromPlaylist,
+  downloadSingleAudio,
+} from '../youtube/youtube.audio.service';
+import {
+  chooseDownloadFormat,
+  downloadSingleVideo,
+} from '../youtube/youtube.video.service';
 
 describe('Youtube service', function () {
+  beforeAll(async () => {
+    await redisClient.connect();
+    await redisClient.flushAll();
+    config();
+  });
+
+  afterAll(async () => {
+    await redisClient.flushAll();
+    await redisClient.disconnect();
+  });
+  const youtubeVideoLink = 'https://www.youtube.com/watch?v=5hPtU8Jbpg0';
+  const playlistId = 'PLcdCdVUWGyjhN6Ea6RL1DX-GpeD8gXmsY';
+  const longPlayListId = 'PLDeVhw7bs_K5_jTmR5AxhKHM5we_x7WKM';
+  describe('getYoutubeContentInfo', function () {
+    test('should get youtube content successfully', async function () {
+      const result = await firstValueFrom(
+        getYoutubeContentInfo(youtubeVideoLink),
+      );
+      expect(result).toBeDefined();
+      assertType<videoInfo | undefined>(result);
+    });
+    test('should throw error video not exist', async function () {
+      await expect(
+        firstValueFrom(getYoutubePlaylistInfo(youtubeVideoLink + 'something')),
+      ).rejects.toThrow();
+    });
+    test('should throw error for timeout', async function () {
+      await setTimeout(() => {}, 5000);
+      await expect(
+        firstValueFrom(getYoutubePlaylistInfo(youtubeVideoLink)),
+      ).rejects.toThrow();
+    });
+  });
+  describe('download single audio', function () {
+    test('should download file successfully(mp3)', async function () {
+      const result = await firstValueFrom(
+        downloadSingleAudio(youtubeVideoLink, false),
+      );
+      expect(result).toBeDefined();
+      expect(result.filePath.endsWith('.mp3')).toBe(true); // because isHighQuality is false
+      assertType<DownloadedAudio>(result);
+    });
+    test('should download file successfully(flac)', async function () {
+      const result = await firstValueFrom(
+        downloadSingleAudio(youtubeVideoLink, true),
+      );
+      expect(result).toBeDefined();
+      expect(result.filePath.endsWith('.flac')).toBe(true); // because isHighQuality is false
+      assertType<DownloadedAudio>(result);
+    });
+    test('should throw error video not exist', async function () {
+      await expect(
+        firstValueFrom(downloadSingleAudio('notexist url', false)),
+      ).rejects.toThrow();
+    });
+  });
+  describe('getYoutubePlaylistInfo', () => {
+    test('should getYoutubePlaylistInfo successfully.', async () => {
+      const result = await firstValueFrom(getYoutubePlaylistInfo(playlistId));
+      expect(result.status).toBe(200);
+      expect(result.data).toBeTypeOf('object');
+      assertType<youtubePlayListResponse>(result.data);
+    });
+
+    test('should getYoutubePlaylistInfo fail not a valid playList id.', async () => {
+      await expect(
+        firstValueFrom(getYoutubePlaylistInfo('someInvalidId')),
+      ).rejects.toThrow();
+    });
+    test('should getYoutubePlaylistInfo fail playlist had more than 50 items.', async () => {
+      await expect(
+        firstValueFrom(getYoutubePlaylistInfo(longPlayListId)),
+      ).rejects.toThrow();
+    });
+  });
+  describe('getPlaylistItemsUrls', () => {
+    test('should getPlaylistItemsUrls', async () => {
+      const result = await firstValueFrom(getPlaylistItemsUrls(playlistId));
+      expect(result).not.toBe([]);
+      expect(result.length).toBeGreaterThan(0);
+      assertType<string[]>(result);
+    });
+    test('should getYoutubePlaylistInfo fail invalid playlist id.', async () => {
+      await expect(
+        firstValueFrom(getPlaylistItemsUrls('invalidPlaylistId')),
+      ).rejects.toThrow();
+    });
+    test('should getYoutubePlaylistInfo fail playlist had more than 50 items.', async () => {
+      await expect(
+        firstValueFrom(getPlaylistItemsUrls(longPlayListId)),
+      ).rejects.toThrow();
+    });
+  });
+  describe('download playlist', () => {
+    test('should downloadAudioFromPlaylist', async () => {
+      const result = await firstValueFrom(
+        downloadAudioFromPlaylist(playlistId, false, 'test-album'),
+      );
+      expect(result).toBeDefined();
+      expect(result).toBeTypeOf('string');
+      expect(result.endsWith('.zip')).toBe(true);
+      unlinkSync(result);
+    }, 60000);
+    test('should downloadAudioFromPlaylist fail invalid playlist id', async () => {
+      await expect(
+        firstValueFrom(
+          downloadAudioFromPlaylist('invalid id', false, 'test-album'),
+        ),
+      ).rejects.toThrow();
+    }, 60000);
+  });
+
+  describe('downloadSingleVideo', () => {
+    test('should download and return a video in the specified quality', async () => {
+      // Replace 'videoId' with the actual ID of the YouTube video you want to test.
+      const videoId = 'TyguvcHwE3s';
+
+      // Replace 'quality' with one of the available video qualities: 'high', 'medium', or 'small'.
+      const quality = 'high';
+
+      // Call the function and subscribe to its observable.
+      const result = await firstValueFrom(
+        downloadSingleVideo(videoId, quality),
+      );
+      expect(result).toBeDefined();
+      expect(result.filePath).toContain('.mp4');
+      expect(result.title).toBeDefined();
+      unlinkSync(result.filePath);
+    });
+    test('should fail if video not exist', async () => {
+      // Replace 'videoId' with the actual ID of the YouTube video you want to test.
+      const videoId = 'TyguvcHwE3t';
+
+      // Replace 'quality' with one of the available video qualities: 'high', 'medium', or 'small'.
+      const quality = 'high';
+
+      // Call the function and subscribe to its observable.
+      downloadSingleVideo(videoId, quality).subscribe({
+        error(err) {
+          expect(err).toBeDefined();
+        },
+      });
+    }, 60000);
+  });
+
+  describe('chooseDownloadFormat', () => {
+    let info: videoInfo;
     beforeAll(async () => {
-        await redisClient.connect();
-        await redisClient.flushAll();
-        config();
+      info = await ytdl.getInfo(youtubeVideoLink);
+    });
+    test('should return the high quality video format when "high" is passed in as quality', () => {
+      const result: {
+        downloadVideoFormat: ytdl.videoFormat;
+        downloadAudioFormat: ytdl.videoFormat;
+      } = chooseDownloadFormat(info, 'high');
+      expect(result.downloadVideoFormat).toEqual({
+        quality: 'hd1080',
+        hasAudio: false,
+        hasVideo: true,
+      });
+      expect(result.downloadAudioFormat).toEqual({
+        quality: 'small',
+        hasAudio: true,
+        hasVideo: true,
+      });
     });
 
-    afterAll(async () => {
-        await redisClient.flushAll();
-        await redisClient.disconnect();
+    test('should return the medium quality video format when "medium" is passed in as quality', () => {
+      const result: {
+        downloadVideoFormat: ytdl.videoFormat;
+        downloadAudioFormat: ytdl.videoFormat;
+      } = chooseDownloadFormat(info, 'medium');
+      expect(result.downloadVideoFormat).toEqual({
+        quality: 'medium',
+        hasAudio: true,
+        hasVideo: true,
+      });
+      expect(result.downloadAudioFormat).toEqual({
+        quality: 'small',
+        hasAudio: true,
+        hasVideo: true,
+      });
     });
-    const youtubeVideoLink = 'https://www.youtube.com/watch?v=5hPtU8Jbpg0';
-    const playlistId = 'PLcdCdVUWGyjhN6Ea6RL1DX-GpeD8gXmsY';
-    const longPlayListId = 'PLxA687tYuMWjdjPnwbOt_Nmgt3jnT1Yqi';
-    describe('getYoutubeContentInfo', function () {
-        test('should get youtube content successfully', async function () {
-            const result = await firstValueFrom(
-                getYoutubeContentInfo(youtubeVideoLink),
-            );
-            expect(result).toBeDefined();
-            assertType<videoInfo | undefined>(result);
-        });
-        test('should throw error video not exist', async function () {
-            await expect(
-                firstValueFrom(getYoutubePlaylistInfo(youtubeVideoLink + 'something')),
-            ).rejects.toThrow();
-        });
-        test('should throw error for timeout', async function () {
-            await setTimeout(() => {
-            }, 5000);
-            await expect(
-                firstValueFrom(getYoutubePlaylistInfo(youtubeVideoLink)),
-            ).rejects.toThrow();
-        });
-    });
-    describe('download single audio', function () {
-        test('should download file successfully(mp3)', async function () {
-            const result = await firstValueFrom(
-                downloadSingleAudio(youtubeVideoLink, false),
-            );
-            expect(result).toBeDefined();
-            expect(result.filePath.endsWith('.mp3')).toBe(true); // because isHighQuality is false
-            assertType<DownloadedAudio>(result);
-        });
-        test('should download file successfully(flac)', async function () {
-            const result = await firstValueFrom(
-                downloadSingleAudio(youtubeVideoLink, true),
-            );
-            expect(result).toBeDefined();
-            expect(result.filePath.endsWith('.flac')).toBe(true); // because isHighQuality is false
-            assertType<DownloadedAudio>(result);
-        });
-        test('should throw error video not exist', async function () {
-            await expect(
-                firstValueFrom(downloadSingleAudio('notexist url', false)),
-            ).rejects.toThrow();
-        });
-    });
-    describe('getYoutubePlaylistInfo', () => {
-        test('should getYoutubePlaylistInfo successfully.', async () => {
-            const result = await firstValueFrom(getYoutubePlaylistInfo(playlistId));
-            expect(result.status).toBe(200);
-            expect(result.data).toBeTypeOf('object');
-            assertType<youtubePlayListResponse>(result.data);
-        });
 
-        test('should getYoutubePlaylistInfo fail not a valid playList id.', async () => {
-            await expect(
-                firstValueFrom(getYoutubePlaylistInfo('someInvalidId')),
-            ).rejects.toThrow();
-        });
-        test('should getYoutubePlaylistInfo fail playlist had more than 15 items.', async () => {
-            await expect(
-                firstValueFrom(getYoutubePlaylistInfo(longPlayListId)),
-            ).rejects.toThrow();
-        });
+    test('should return the small quality video format when any other value is passed in as quality', () => {
+      const result: {
+        downloadVideoFormat: ytdl.videoFormat;
+        downloadAudioFormat: ytdl.videoFormat;
+      } = chooseDownloadFormat(info, 'low');
+      expect(result.downloadVideoFormat).toEqual({
+        quality: 'small',
+        hasAudio: true,
+        hasVideo: true,
+      });
+      expect(result.downloadAudioFormat).toEqual({
+        quality: 'small',
+        hasAudio: true,
+        hasVideo: true,
+      });
     });
-    describe('getPlaylistItemsUrls', () => {
-        test('should getPlaylistItemsUrls', async () => {
-            const result = await firstValueFrom(getPlaylistItemsUrls(playlistId));
-            expect(result).not.toBe([]);
-            expect(result.length).toBeGreaterThan(0);
-            assertType<string[]>(result);
-        });
-        test('should getYoutubePlaylistInfo fail invalid playlist id.', async () => {
-            await expect(
-                firstValueFrom(getPlaylistItemsUrls('invalidPlaylistId')),
-            ).rejects.toThrow();
-        });
-        test('should getYoutubePlaylistInfo fail playlist had more than 15 items.', async () => {
-            await expect(
-                firstValueFrom(getPlaylistItemsUrls(longPlayListId)),
-            ).rejects.toThrow();
-        });
-    });
-    describe('download playlist', () => {
-        test('should downloadAudioFromPlaylist', async () => {
-            const result = await firstValueFrom(
-                downloadAudioFromPlaylist(playlistId, false, 'test-album'),
-            );
-            expect(result).toBeDefined();
-            expect(result).toBeTypeOf('string');
-            expect(result.endsWith('.zip')).toBe(true);
-            unlinkSync(result);
-        }, 60000);
-        test('should downloadAudioFromPlaylist fail invalid playlist id', async () => {
-            await expect(
-                firstValueFrom(
-                    downloadAudioFromPlaylist('invalid id', false, 'test-album'),
-                ),
-            ).rejects.toThrow();
-        }, 60000);
-    });
+  });
 });
--- apps/server/src/services/youtube/youtube.video.service.ts
+++ apps/server/src/services/youtube/youtube.video.service.ts
@@ -1,65 +1,89 @@
-import {catchError, from, mergeMap, Observable, switchMap, toArray} from "rxjs";
-import {getCachedVideo} from "../redis.service";
-import ytdl, {videoInfo} from "ytdl-core";
-import {v4 as uuidv4} from "uuid";
-import {PassThrough} from "stream";
-import createHttpError from "http-errors";
-import {DownloadedAudio, DownloadedVideo} from "../../interfaces/download.interface";
-import {zipDownloadedContent} from "../archive.service";
-import {getPlaylistItemsUrls} from "./youtube.service";
-import {downloadSingleAudio} from "./youtube.audio.service";
-import {createWriteStream} from "fs";
-import {mergeAudioWithVideo} from "../convertor.service";
+import {
+  catchError,
+  from,
+  mergeMap,
+  Observable,
+  switchMap,
+  toArray,
+} from 'rxjs';
+import { getCachedVideo } from '../redis.service';
+import ytdl, { videoInfo } from 'ytdl-core';
+import { v4 as uuidv4 } from 'uuid';
+import { PassThrough } from 'stream';
+import createHttpError from 'http-errors';
+import {
+  DownloadedAudio,
+  DownloadedVideo,
+} from '../../interfaces/download.interface';
+import { zipDownloadedContent } from '../archive.service';
+import { getPlaylistItemsUrls } from './youtube.service';
+import { createWriteStream } from 'fs';
+import { mergeAudioWithVideo } from '../convertor.service';
 
+// Define the function to download a single video
 export function downloadSingleVideo(
-    videoId: string,
-    quality: string,
+  videoId: string,
+  quality: string,
 ): Observable<DownloadedVideo> {
-    return new Observable((subscriber) => {
-        getCachedVideo(videoId)
-            .pipe(
-                mergeMap(async (info: videoInfo) => {
-                    const {downloadVideoFormat, downloadAudioFormat} = chooseFormat(info, quality)
-                    const filePath = `${__dirname}/../../../public/${uuidv4()}.mp4`;
-                    const title = info.videoDetails.title;
-                    const result = new PassThrough({highWaterMark: 100 || 1024 * 512});
-                    const videoStream = ytdl
-                        .downloadFromInfo(info, {
-                            format: downloadVideoFormat,
-                        })
-                    const audioStream = ytdl
-                        .downloadFromInfo(info, {
-                            format: downloadAudioFormat,
-                        })
-                    mergeAudioWithVideo(audioStream, videoStream).pipe(result)
-                    const data = result.pipe(createWriteStream(filePath))
-                    subscriber.next({data,filePath,title})
-                    subscriber.complete()
-                }),
-            )
-            .subscribe();
-    });
+  // Return an observable
+  return new Observable((subscriber) => {
+    // Pipe the cached video info to process it further
+    getCachedVideo(videoId)
+      .pipe(
+        mergeMap(async (info: videoInfo) => {
+          // Choose the appropriate video and audio formats based on the provided quality
+          const { downloadVideoFormat, downloadAudioFormat } = chooseDownloadFormat(
+            info,
+            quality,
+          );
+          // Set the file path to save the downloaded video
+          const filePath = `${__dirname}/../../../public/${uuidv4()}.mp4`;
+          // Get the title of the video
+          const title = info.videoDetails.title;
+          // Create a stream for the final output
+          const result = new PassThrough({ highWaterMark: 100 || 1024 * 512 });
+          // Create a stream for the video content
+          const videoStream = ytdl.downloadFromInfo(info, {
+            format: downloadVideoFormat,
+          });
+          // Create a stream for the audio content
+          const audioStream = ytdl.downloadFromInfo(info, {
+            format: downloadAudioFormat,
+          });
+          // Merge the audio and video streams into one
+          mergeAudioWithVideo(audioStream, videoStream).pipe(result);
+          // Save the merged stream to file
+          const data = result.pipe(createWriteStream(filePath));
+          // Send the downloaded video's data, file path, and title through the observable
+          subscriber.next({ data, filePath, title });
+          // Complete the observable
+          subscriber.complete();
+        }),
+      )
+      .subscribe();
+  });
 }
 
+export function chooseDownloadFormat(
+  info: videoInfo,
+  quality: string,
+): {
+  downloadVideoFormat: ytdl.videoFormat;
+  downloadAudioFormat: ytdl.videoFormat;
+} {
+  const { formats } = info;
+  const videoFilter = (quality === 'high' && ['hd1080', 'hd720']) ||
+    (quality === 'medium' && ['medium']) || ['small'];
 
-function chooseFormat(info: videoInfo, quality: string) {
-    const videoFormats = info.formats;
-    const downloadVideoFormat =
-        quality === 'high'
-            ? videoFormats.find(
-                (fo) =>
-                    (fo.quality === 'hd1080' || fo.quality === 'hd720') &&
-                    !fo.hasAudio,
-            )
-            : quality === 'medium'
-                ? videoFormats.find(
-                    (fo) => fo.quality === 'medium' && !fo.hasAudio,
-                )
-                : videoFormats.find(
-                    (fo) => fo.quality === 'small' && !fo.hasAudio,
-                );
-    const downloadAudioFormat = videoFormats.find((fo) => !fo.hasVideo);
-    return {downloadVideoFormat, downloadAudioFormat}
+  const downloadVideoFormat = formats.find(
+    ({ quality, hasAudio }) => videoFilter.includes(quality) && !hasAudio,
+  );
+
+  const downloadAudioFormat = formats.find(({ hasVideo }) => !hasVideo);
+  if (!downloadAudioFormat || !downloadVideoFormat) {
+    throw new Error('This video does not has this quality');
+  }
+  return { downloadVideoFormat, downloadAudioFormat };
 }
 
 /**
@@ -71,31 +95,31 @@ function chooseFormat(info: videoInfo, quality: string) {
  * @returns {Observable<string>} - Observable that emits the path of the zip file when complete.
  */
 export function downloadVideoFromPlaylist(
-    playlistId: string,
-    quality: string,
-    albumName: string,
+  playlistId: string,
+  quality: string,
+  albumName: string,
 ): Observable<string> {
-    /**
-     * Extracted function for generic error handling.
-     */
-    const handleErrors = (step: string) => (err: any) => {
-        console.error(`Error occurred while ${step}: ${err}`);
-        throw createHttpError(err);
-    };
+  /**
+   * Extracted function for generic error handling.
+   */
+  const handleErrors = (step: string) => (err: any) => {
+    console.error(`Error occurred while ${step}: ${err}`);
+    throw createHttpError(err);
+  };
 
-    return getPlaylistItemsUrls(playlistId).pipe(
-        switchMap((videoIds) =>
-            from(videoIds).pipe(
-                mergeMap((videoId) => downloadSingleVideo(videoId,quality)),
-                toArray(),
-                catchError(handleErrors('downloading audios')),
-            ),
-        ),
-        mergeMap((downloadedVideos: DownloadedVideo[]) =>
-            zipDownloadedContent(downloadedVideos, quality, albumName).pipe(
-                catchError(handleErrors('zipping downloaded audios')),
-            ),
-        ),
-        catchError(handleErrors('getting playlist items URLs')),
-    );
+  return getPlaylistItemsUrls(playlistId).pipe(
+    switchMap((videoIds) =>
+      from(videoIds).pipe(
+        mergeMap((videoId) => downloadSingleVideo(videoId, quality)),
+        toArray(),
+        catchError(handleErrors('downloading audios')),
+      ),
+    ),
+    mergeMap((downloadedVideos: DownloadedVideo[]) =>
+      zipDownloadedContent(downloadedVideos, quality, albumName).pipe(
+        catchError(handleErrors('zipping downloaded audios')),
+      ),
+    ),
+    catchError(handleErrors('getting playlist items URLs')),
+  );
 }
